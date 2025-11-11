61ad2a0aa9eece8ef318b8db484118531cc4085d
Perry Higginbotham <perryhigginbotha@u.boisestate.edu>
2025-11-11 10:20:45 -0700

    Add files via upload

---
+++ Week 12/one_hot.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/11/2025 09:36:48 AM
+// Design Name: 
+// Module Name: one_hot
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module one_hot(
+
+    );
+endmodule
